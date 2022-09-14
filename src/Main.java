855ecf6dd0cca56f674ed0cb28c9ca11de810432
Viktorryy <viktory_o@rambler.ru>
2022-09-14 23:38:29 +0400

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte bbb = 12;
-        short sss = 10325;
-        int iii = 123456789;
-        long lll = 123456789;
-        float fff = 0.123f;
-        double ddd = 123456.1234567890123456789;
+        // Задача 8
 
-        System.out.println("Значение переменной  bbb  с типом  byte  равно " + bbb);
-        System.out.println("Значение переменной  sss  с типом short равно " + sss);
-        System.out.println("Значение переменной  iii  с типом int  равно " + iii);
-        System.out.println("Значение переменной  lll  с типом long равно " + lll);
-        System.out.println("Значение переменной fff с типом float равно " + fff);
-        System.out.println("Значение переменной  ddd  с типом double равно " + ddd);
+
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKris = 76230;
+
+        float promotion = (float) 10 /100;
+        float promotion1 = promotion + 1;
+
+        float salaryMashaNew = (float)salaryMasha * promotion1;
+        float salaryDenisNew =(float)salaryDenis * promotion1;
+        float salaryKrisNew = (float)salaryKris * promotion1;
+
+        float salaryMashaProm = (float)salaryMasha * promotion * 12;
+        float salaryDenisProm =(float)salaryDenis * promotion * 12;
+        float salaryKrisProm = (float)salaryKris * promotion * 12;
+
+
+        System.out.println("Маша теперь получает " + salaryMashaNew + " рублей. " +
+                "Годовой доход вырос на " + salaryMashaProm + " рублей");
+        System.out.println("Денис теперь получает " + salaryDenisNew + " рублей. " +
+                "Годовой доход вырос на " + salaryDenisProm + " рублей");
+        System.out.println("Кристина теперь получает " + salaryKrisNew + " рублей. " +
+                "Годовой доход вырос на " + salaryKrisProm + " рублей");
 
 
     }
